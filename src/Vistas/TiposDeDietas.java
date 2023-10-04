43cb67a840484c90cb01fe2ef2ff3d99106dd9f6
purplefox1977 <141675561+purplefox1977@users.noreply.github.com>
2023-10-04 14:50:48 -0300

    algunas vistas para revisar

---
+++ src/Vistas/TiposDeDietas.java
@@ -0,0 +1,20 @@
+/*
+ https://www.nestle-contigo.co/elige-a-tu-medida/tipos-de-dieta
+
+ */
+package Vistas;
+
+/**
+ *
+ * @author Loboplateado77
+ */
+public enum TiposDeDietas {
+    Dieta_omnívora,
+    Dieta_vegetariana,
+    Dieta_mediterránea,
+    Dieta_modificada_en_calorías,
+    Dieta_modificada_en_proteína,
+    Dieta_libre_de_gluten,
+    Dieta_DASH
+    
+}
